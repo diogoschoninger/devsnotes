a565ad89606d9f246025eb6a2c3c594ed77b36ad
Diogo Schoninger <schoninger.diogo@gmail.com>
2022-03-25 16:51:52 -0300

    202203251651

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,16 @@
-import './App.css';
+import { useCallback, useEffect } from "react";
 
 function App() {
+  const test = useCallback(async () => {
+    await fetch("http://localhost/devsnotes/api/ping.php")
+    .then(response => response.json())
+    .then(response => console.log(response));
+  }, [])
+
+  useEffect(() => {
+    test();
+  }, [test]);
+
   return (
     <div></div>
   );
